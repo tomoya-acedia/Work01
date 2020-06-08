bfdc695871a4c787b11187e2ad11313007db9425
TomoyaIizuka <30188@ojs.ac.jp>
2020-06-08 13:58:53 +0900

    新規登録

--- SampleSC/CodeFile1.cs
+++ SampleSC/CodeFile1.cs
@@ -1,4 +1,15 @@
-﻿class Sample
+﻿using System.Windows.Forms;
+
+
+
+class Sample
 {
+    static Form form;
+
+    static void Maimn()
+    {
+        form = new Form();
+        Application.Run(form);
+    }
 
 }
