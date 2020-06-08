4bc55d0a37474721bbfda644f6827f83148fd4f6
TomoyaIizuka <30188@ojs.ac.jp>
2020-06-08 15:09:43 +0900

    for (int i = 0; i < scores.Length; i++)         {             scores[i] = 10 * (i + 1); //10 * i + 10;             Console.WriteLine(scores[i]);         }

--- SampleSC/Sample.cs
+++ SampleSC/Sample.cs
@@ -12,12 +12,17 @@ class Sample:Form
     //デフォルトコンストラクタ(引数のないコンストラクタ)
     public Sample()
     {
-        var socres = new int[] { 65, 54, 78, 96, 81 };
-        //int[] scores = new int[5];
+        //var socres = new int[] { 65, 54, 78, 96, 81 };
+        int[] scores = new int[5];
         //int [] scores = { 65, 54, 78, 96, 81 };
-        for (int i = 0; i < socres.Length; i++)
+        //for (int i = 0; i < socres.Length; i++)
+        //{
+        //    Console.WriteLine(socres[i]);
+        //}
+        for (int i = 0; i < scores.Length; i++)
         {
-            Console.WriteLine(socres[i]);
+            scores[i] = 10 * (i + 1); //10 * i + 10;
+            Console.WriteLine(scores[i]);
         }
     }
 }
