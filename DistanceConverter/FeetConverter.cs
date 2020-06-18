6be742db663e9acac9946c599710d30346f0e9b4
TomoyaIizuka <30188@ojs.ac.jp>
2020-06-18 17:43:04 +0900

    計算処理をクラスとして分離

---
+++ DistanceConverter/FeetConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter
+{
+    //フィートとメートルの単位変換クラス
+    public class FeetConverter
+    {
+        //メートルからフィートを求める
+        public double FromMeter(double meter)
+        {
+            return meter * 3.28084;
+        }
+
+        //フィートからメートルを求める
+        public double ToMeter(double feet)
+        {
+            return feet * 0.3048;
+        }
+
+    }
+}
--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter
 {
+   
     class Program
     {
         //-tom フィートからメートルへの対応表
@@ -24,23 +25,23 @@ namespace DistanceConverter
             }
         }
 
+        static FeetConverter feetConverter = new FeetConverter();
+
         //フィートからメートルへの対応表を出力
-        //引数:start 最初の値
-        //     stop  最後の値
         static void PrintFeetToMeter(int start,int stop)
         {
             for (int feet = start; feet <= stop; feet++)
             {
-                Console.WriteLine("{0}ft = {1:0.0000}", feet, feet * 0.3048);
+                Console.WriteLine("{0}ft = {1:0.0000}", feet, feetConverter.ToMeter(feet));
             }
         }
 
         //メートルからフィートへの対応表を出力
         static void PrintMeterToFeetList(int start, int stop)
         {
-            for (int feet = start; feet <= stop; feet++)
+            for (int meter = start; meter <= stop; meter++)
             {
-                Console.WriteLine("{0}ft = {1:0.0000}", feet, feet * 3.28084);
+                Console.WriteLine("{0}ft = {1:0.0000}", meter, feetConverter.FromMeter(meter));
             }
         }
     }
