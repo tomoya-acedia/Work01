21944c0be22175048ffa8f672c0df1f237583e5a
TomoyaIizuka <30188@ojs.ac.jp>
2020-06-22 15:29:14 +0900

    売上集計処理(ファイル読み込み) 作成

--- DistanceConverter/FeetConverter.cs
+++ DistanceConverter/FeetConverter.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DistanceConverter
-{
+﻿using System;                         
+using System.Collections.Generic;     
+using System.Linq;                    
+using System.Text;                    
+using System.Threading.Tasks;         
+                                      
+namespace DistanceConverter           
+{                                     
     //フィートとメートルの単位変換クラス
-    public static class FeetConverter
-    {
+    public static class FeetConverter 
+    {                                 
+        private const double retio = 0.3048;
         //メートルからフィートを求める(静的メソッドとして定義)
         public static double FromMeter(double meter)
-        {
-            return meter * 3.28084;
-        }
-
+        {                             
+            return meter / retio;     
+        }                             
+                                      
         //フィートからメートルを求める(静的メソッドとして定義)
         public static double ToMeter(double feet)
-        {
-            return feet * 0.3048;
-        }
+        {                             
+            return feet * retio;      
+        }                             
 
     }
 }
---
+++ SalesManagement/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement
+{
+    class Program
+    {
+        //svmで↓が出る(ショートカットキー)
+        static void Main(string[] args)
+        {
+            SalesCounter sales = new SalesCounter(ReadSales("Sales.csv"));
+        }
+
+        //ctorで↓が出る(ショートカットキー)
+        public Program()
+        {
+
+        }
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(string filePath)
+        {
+            //売上データを入れるリストオブジェクトを生成
+            var sales = new List<Sale>();
+
+            //ファイルから読み込み
+            string[] lines = File.ReadAllLines(filePath);
+            
+            //読み込んだ行だけ繰り返す
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(',');
+
+                Sale sale = new Sale
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);//saleオブジェクトをリストに追加
+            }
+            return sales;
+        }
+
+    }
+
+}
